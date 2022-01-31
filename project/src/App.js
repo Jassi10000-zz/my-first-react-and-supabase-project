9374c4663fcfbdac581b3449d202b8527967f54b
Jaskeerat Singh <jaskeerat10000@gmail.com>
2022-01-31 23:51:28 +0530

    Revert "completed the application ☘ 🎉"
    This reverts commit 2666d48af849111c520321e0b5df8b8e4b51e871.

--- project/src/App.js
+++ project/src/App.js
@@ -1,62 +1,25 @@
-import { useState , useEffect } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { supabaseClient } from './client';
 
 function App() {
-
-  // creating the user state
-  const [ user , setUser ] = useState(null);
-
-  // configuring useEffect
-  useEffect(() => {
-
-    // calling the authorizeUser function
-    authorizeUser();
-
-    // window event listener to recall the user after the OAuth redirect happens
-    window.addEventListener('hashchange', function(){
-      authorizeUser();
-    });
-  },[]);
-
-  async function authorizeUser() {
-    const user = supabaseClient.auth.user();
-    setUser(user);
-  }
-
-  // function to sign in with github 
-  async function signInWithGithub() {
-    await supabaseClient.auth.signIn({
-      provider: 'github',
-    });
-  }
-
-  // function to sign out
-  async function signout(){
-    await supabaseClient.auth.signOut();
-    setUser(null);
-  }
-
-  // if the user exists
-  if(user){
-    return (
-      <div className="App">
-        <div className="card">
-              <p>Hey {user.email} </p>
-              <button onClick={signout}>Sign Out</button>
-        </div>
-      </div>
-    )
-  }
-  
   return (
     <div className="App">
-      <div className="card">
-            <p>Hey new user , please sign in </p>
-            <button onClick={signInWithGithub}>Sign In</button>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
 export default App;
